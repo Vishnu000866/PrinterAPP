cd9802142c86928ea527a2c719534c3d59d4239b
Vishnu000866 <chaudharibishnu11@gmail.com>
2024-10-18 21:48:51 +0100

    declaring two variables and assigning their values

--- printerApp.java
+++ printerApp.java
@@ -6,6 +6,11 @@ public class printerApp {
     public static void main(String[] args) {
         System.out.println("Printer App");
         System.out.println("This app is made by:");
-        String creater;
+        String creator;
+        creator="Bishnu chaudhary";
+        System.out.println("This app is made by: "+creator);
+        int number1 = 2;
+        int number2 = 3;
+        
     }
 }

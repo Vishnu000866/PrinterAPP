146e80adbfe4f4738157151a40f48887665e10ad
Vishnu000866 <chaudharibishnu11@gmail.com>
2024-10-18 21:43:49 +0100

    declaring string type varaiable called creater

--- printerApp.java
+++ printerApp.java
@@ -4,6 +4,8 @@
 public class printerApp {
 
     public static void main(String[] args) {
-        
+        System.out.println("Printer App");
+        System.out.println("This app is made by:");
+        String creater;
     }
 }

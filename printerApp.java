6903bca0b8d94d9e339b462d1c5f0e6f465b9246
Vishnu000866 <chaudharibishnu11@gmail.com>
2024-10-18 22:01:09 +0100

    changing the values and printering multiplication

--- printerApp.java
+++ printerApp.java
@@ -9,8 +9,15 @@ public class printerApp {
         String creator;
         creator="Bishnu chaudhary";
         System.out.println("This app is made by: "+creator);
-        int number1 = 2;
-        int number2 = 3;
-        
+        int number1 = 5;
+        int number2 = 8;
+        //System.out.println(number1 );
+       System.out.println("The value of number1 is: "+number1);
+       System.out.println("The value of number1 is: "+number2);
+       int multiplication;
+       multiplication=number1*number2;
+       System.out.println("The multiplication is: "+multiplication);
+       System.out.println(number1+" * "+number2+" = "+multiplication);
+
     }
 }
